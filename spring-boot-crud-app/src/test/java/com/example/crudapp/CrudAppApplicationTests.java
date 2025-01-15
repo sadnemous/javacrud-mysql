3bd5df1efbea553e3d0376475cb5685ad67140b9
2soumen.d@gmail.com <2soumen.d@gmail.com>
2025-01-15 13:32:23 -0600

    4th commit

---
+++ spring-boot-crud-app/src/test/java/com/example/crudapp/CrudAppApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.crudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
